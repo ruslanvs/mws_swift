c607fa3641c7ce0f9b88695e630b2f9d1ccb0156
ruslanvs <ruslanvs@yahoo.com>
2018-04-20 10:36:51 -0700

    With seed data

--- mws_swift/DataModel.swift
+++ mws_swift/DataModel.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2018 Ruslan Suvorov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class SchoolModel {
+    private var managedObjectContext = ( UIApplication.shared.delegate as! AppDelegate ).persistentContainer.viewContext
+    private var appDelegate = ( UIApplication.shared.delegate as! AppDelegate )
+    
+    static let shared = SchoolModel()
+    
+    func getAll() -> [School] {
+        let request = NSFetchRequest<NSFetchRequestResult>( entityName: "School" )
+        
+        do {
+            return try managedObjectContext.fetch( request ) as! [School]
+        } catch {
+            print ( error )
+            return []
+        }
+        
+    }
+    
+    func create( title: String ) -> School {
+        let item = NSEntityDescription.insertNewObject(forEntityName: "School", into: managedObjectContext ) as! School
+        item.title = title
+        saveContext()
+        return item
+    }
+    
+    func saveContext() {
+        appDelegate.saveContext()
+    }
+}
--- mws_swift/HomeVC.swift
+++ mws_swift/HomeVC.swift
@@ -11,13 +11,22 @@ import UIKit
 class HomeVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-//    var tableData = 
+    var tableData = SchoolModel.shared.getAll()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        seedData()
+    }
+    
+    func seedData() {
+        let schoolTitles = ["Hogwarts", "Beauxbatons", "Castelobruxo", "Durmstrang Institute", "Ilvermorny"]
+        for title in schoolTitles {
+            SchoolModel.shared.create(title: title)
+        }
+        tableData = SchoolModel.shared.getAll()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,12 +38,12 @@ class HomeVC: UIViewController {
 extension HomeVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableCell", for: indexPath)
-        cell.textLabel?.text = "Table test"
+        cell.textLabel?.text = tableData[indexPath.row].title
         return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tableData.count
     }
 
 }
